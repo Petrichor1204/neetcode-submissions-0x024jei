19ab4934a9298a70011d836fe5b249e6c2b1e065
Esi Abrokwa <127475467+Petrichor1204@users.noreply.github.com>
2026-05-18 14:41:26 -0400

    Add: car-fleet - submission-3

---
+++ Data Structures & Algorithms/car-fleet/submission-3.py
@@ -0,0 +1,37 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        # target = 10, position = [4,1,0,7], speed = [2,2,1,1]
+        # pairs = [[7,1], [4,2], [1,2], [0,1]]
+        # stack = [3, 4.5, 10]
+        # times = [10, 4.5, 3, 3]
+
+        fleet = []
+        pairs = []
+        # pair up positions and speeds
+        for i in range(len(position)):
+            pairs.append([position[i], speed[i]])
+
+        # sort them in desc order
+        pairs.sort(reverse=True)
+
+        # loop through pairs
+        # for each pair, calculate its time to target
+        for p,s in pairs:
+            time = (target - p)/s
+            # add time unconditionally
+            fleet.append(time)
+
+            #if the time we just added is <= the first item, then remove cuz it forms a fleet
+            if len(fleet) >= 2 and fleet[-1] <= fleet[-2]:
+                fleet.pop()
+
+        return len(fleet)
+        
+            
+            
+            
+            
+      
+
+        
+       
