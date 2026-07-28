b8b0e833f808e65bbf10c3037380d0f63ac8534f
Esi Abrokwa <127475467+Petrichor1204@users.noreply.github.com>
2026-07-28 10:50:18 -0500

    Add: n-th-tribonacci-number - submission-2

---
+++ Data Structures & Algorithms/n-th-tribonacci-number/submission-2.py
@@ -0,0 +1,21 @@
+class Solution:
+    def tribonacci(self, n: int) -> int:
+        # goal: return the value of Tn which is the sum of the three terms before it
+        # Tn = T(n - 1) +  (T(n - 1) - T(n - 4))
+        # to get the val of Tn, we need to get the sum of the three vals
+        # before it which is the term right before it plus the tow before it
+        # the term right before it already has the sum of of the tree vals so we can just deduct the n - 3 term
+        #[0,1,1,0,0,0]
+        #       i 
+        
+        if n == 0:
+            return 0
+        if n == 1 or n == 2:
+            return 1 
+        
+        a, b, c = 0, 1, 1
+        
+        for i in range(3, n + 1):
+            a, b, c = b, c, a + b + c
+
+        return c
