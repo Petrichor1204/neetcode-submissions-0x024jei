e1dbce3b4a537b49f6de264af8cab90c0c97bd3b
Esi Abrokwa <127475467+Petrichor1204@users.noreply.github.com>
2026-07-30 16:15:05 -0500

    Add: baseball-game - submission-2

---
+++ Data Structures & Algorithms/baseball-game/submission-2.py
@@ -0,0 +1,25 @@
+class Solution:
+    def calPoints(self, operations: List[str]) -> int:
+        # goal: is to take a list of operations, perform the operations and then return 
+        # the sum of the result list
+        #  ops=["5","-2","4","C","D","9","+","+"]
+        #                                     i
+        # [5,-2,4,9,13]
+        stack = []
+        for char in operations:
+            # if char is num, add to stack
+            if char not in "CD+":
+                stack.append(int(char))
+            # if char is plus, add prev two
+            elif char == "+":
+                stack.append(stack[-1] + stack[-2])
+            # if char is a D, take last thing and * 2
+            elif char == "D":
+                stack.append(stack[-1] * 2)
+            # if char is C, pop last thing in stack
+            elif char == "C":
+                stack.pop()
+
+
+        # return sum of result
+        return sum(stack)
