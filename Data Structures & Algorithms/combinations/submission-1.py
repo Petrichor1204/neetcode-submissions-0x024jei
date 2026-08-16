057e9cc8f9e8dec59a844af87030fad703b5fef3
Esi Abrokwa <127475467+Petrichor1204@users.noreply.github.com>
2026-08-16 17:48:20 -0500

    Add: combinations - submission-1

---
+++ Data Structures & Algorithms/combinations/submission-1.py
@@ -0,0 +1,24 @@
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        # find all k possible combinations of n numbers
+        # [1,2,3]
+       
+        result = []
+        def dfs(i, subset):
+            # termination
+            if len(subset) == k:
+                result.append(list(subset))
+                return 
+                
+            # continuation
+            for j in range(i, n): 
+                subset.append(j+1)
+                dfs(j + 1, subset)
+                subset.pop()
+
+
+        dfs(0, [])
+        return result
+    # i = 1, j = 1
+    # dfs(0, [1]) dfs(1, [1,2]) dfs(2, [1,2])
+    # [[1,2],]
